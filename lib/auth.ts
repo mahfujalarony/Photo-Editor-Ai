532164b5985da6aa1310e4223d52077edd832aa8
Mahfuj Alam Rony <149530147+mahfujalarony@users.noreply.github.com>
2026-05-02 00:50:21 +0600

    fix build

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,6 @@ import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
-  trustHost: true,
   providers: [
     GoogleProvider({
       clientId: process.env.Google_CLIENT_ID ?? "",
